0b8b5ab659b7b52a0fc4c9b854e0defe587599af
apexcoverage <getapexcoverage@gmail.com>
2026-09-20 18:53:10 -0400

    Change phone input placeholder text
    Updated phone input placeholder to a new example number.

--- components/QuoteForm.tsx
+++ components/QuoteForm.tsx
@@ -82,7 +82,7 @@ export default function QuoteForm() {
         </div>
         <div>
           <label className="text-sm">Phone</label>
-          <input name="phone" required className="w-full mt-1 border rounded-md px-3 py-2" placeholder="(540) 699-0505" />
+          <input name="phone" required className="w-full mt-1 border rounded-md px-3 py-2" placeholder="844-398-2739" />
         </div>
         <div>
           <label className="text-sm">ZIP</label>
