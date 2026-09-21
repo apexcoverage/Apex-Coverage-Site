b0dcbeb2c1a6dfa3e920dd2e377fb69e27a32355
apexcoverage <getapexcoverage@gmail.com>
2026-09-20 20:24:22 -0400

    Add files via upload

---
+++ components/ConsentDisclosure.tsx
@@ -0,0 +1,24 @@
+export default function ConsentDisclosure() {
+  return (
+    <span>
+      Apex Pro Services (doing business as Apex Coverage) would like your
+      consent to send customer care text message communications from (844)
+      398-2739 to your mobile number listed above, in response to your questions
+      or to provide information relevant to your relationship with us.
+      <br />
+      <br />
+      Consent is not a condition of purchase. Message frequency varies. Message
+      and data rates may apply. Reply 'STOP' to unsubscribe at any time. Reply
+      'HELP' for assistance or more information. We do not share your mobile
+      opt-in information with anyone. Our combined Privacy Policy and Messaging
+      Terms and Conditions are available at{" "}
+      <a
+        className="font-semibold text-[#cc0000] underline"
+        href="https://www.driveapexcoverage.com/privacy"
+      >
+        https://www.driveapexcoverage.com/privacy
+      </a>
+      .
+    </span>
+  );
+}
